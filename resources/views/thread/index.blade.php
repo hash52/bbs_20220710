6e9129cae28b223a1fd0bc9445fe7a4b0581ecf0
hash52 <hirokinoakaunto@gmail.com>
2022-07-10 06:46:11 +0000

    スレッド一覧表示

--- resources/views/thread/index.blade.php
+++ resources/views/thread/index.blade.php
@@ -5,7 +5,6 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">スレッド作成</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ action('ThreadController@create')}}">
                         @csrf
@@ -34,6 +33,18 @@
                         </div>
                     </form>
                 </div>
+            </div>
+            <div class="my-3 p-3 bg-white rounded shadow-sm">
+                <h6 class="border-bottom border-gray pb-2 mb-0">スレッド一覧</h6>
+                @foreach($threads as $thread)
+                    <div class="media text-muted pt-3">
+                      <img data-src="holder.js/32x32?theme=thumb&amp;bg=007bff&amp;fg=007bff&amp;size=1" alt="" class="mr-2 rounded">
+                      <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                        <strong class="d-block text-gray-dark">{{ $thread->title }}</strong>
+                        {{ $thread->body }}
+                      </p>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
